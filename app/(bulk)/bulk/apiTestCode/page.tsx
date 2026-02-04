6e57745d9eaa147feb5eab47afefd61ad1f88f29
NPWE164\m_orihara <m_orihara@freestyles.jp>
2026-02-04 13:40:51 +0900

    fix: UI の調整

--- app/(bulk)/bulk/apiTestCode/page.tsx
+++ app/(bulk)/bulk/apiTestCode/page.tsx
@@ -1,8 +1,12 @@
 export default function ApiTestCodePage() {
   return (
-    <div>
-      <h1 className="text-xl font-semibold">一括テストコード生成</h1>
-      <p className="text-muted-foreground">ここに生成UIを置く</p>
+    <div className="flex flex-col h-full overflow-hidden">
+      <header className="border-b w-full mb-2">
+        <h1 className="text-xl font-semibold">一括APIテストコード生成</h1>
+        <p className="text-muted-foreground">ここに生成UIを置く</p>
+      </header>
+
+      <div className="flex h-full gap-2 min-w-0"></div>
     </div>
   );
 }
--- app/(bulk)/bulk/unitTestCode/page.tsx
+++ app/(bulk)/bulk/unitTestCode/page.tsx
@@ -8,7 +8,7 @@ export default function UnitTestCodePage() {
   return (
     <div className="flex flex-col h-full overflow-hidden">
       <header className="border-b w-full mb-2">
-        <h1 className="text-xl font-semibold">一括テストコード生成</h1>
+        <h1 className="text-xl font-semibold">一括単体テストコード生成</h1>
         <p className="text-muted-foreground">ここに生成UIを置く</p>
       </header>
 
